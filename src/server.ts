bb6296c11a4920bc86349f46bc34bcb1f27c941e
mario mendonca <mariomeendonca00@gmail.com>
2022-07-14 22:08:46 -0300

    add cors

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import express from 'express'
 import { router } from './routes'
+import cors from 'cors'
 
 const app = express()
 app.use(express.json())
 app.use(router)
+app.use(cors({
+  origin: '*'
+}))
 
 const PORT = process.env.PORT || 3333
 app.listen(PORT, () => console.log(`🍫 Server running on ${PORT} 🍫`))
