ed4fa6ba2bb4f5ec0ca7c3c49a3010b40df1912b
mario mendonca <mariomeendonca00@gmail.com>
2022-07-15 11:57:34 -0300

    cors working

--- src/server.ts
+++ src/server.ts
@@ -4,10 +4,10 @@ import cors from 'cors'
 
 const app = express()
 app.use(express.json())
-app.use(router)
 app.use(cors({
   origin: '*'
 }))
+app.use(router)
 
 const PORT = process.env.PORT || 3333
 app.listen(PORT, () => console.log(`🍫 Server running on ${PORT} 🍫`))
